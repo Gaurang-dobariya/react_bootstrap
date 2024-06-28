82fde719e9a4d8b6eae30b58383d39c2af12f008
Gaurang-dobariya <gaurangdobariya870@gmail.com>
2024-06-28 19:36:48 +0530

    bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import Future from './Components/Future'
+import Services from './Components/Services'
+import Point from './Components/Point'
+import Key_point from './Components/Key_point'
+import Work from './Components/Work'
+import Testimonial from './Components/Testimonial'
+import Blog from './Components/Blog'
+import Footer from './Components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Navbar/>
+        <Banner/>
+        <Future/>
+        <Services/>
+        <Point/>
+        <Key_point/>
+        <Work/>
+        <Testimonial/>
+        <Blog/>
+        <Footer/>
+
+    </>
+  )
 }
 
-export default App;
+export default App
